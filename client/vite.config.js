29bd69998834b7a347d4aee5a2583908913d9755
“aryan <aryankardam50275@gmail,com>
2025-07-17 17:44:50 +0530

    fix: add base URL for backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  define: {
-    'import.meta.env.VITE_RAZORPAY_KEY_ID': JSON.stringify('rzp_test_yTkQHHvZNL1jxL'),
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_BASE_URL,
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on current mode (e.g., development or production)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      'import.meta.env.VITE_RAZORPAY_KEY_ID': JSON.stringify(env.VITE_RAZORPAY_KEY_ID),
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       }
     }
-  }
-})
+  };
+});
