d1c658e43e252aecad0437fe227f874a917730ca
“aryan <aryankardam50275@gmail,com>
2025-07-17 10:17:11 +0530

    📄 Updated codes

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,19 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const {
-//   createOrder,
-//   verifyPayment,
-//   getPaymentDetails,
-//   paymentFailed
-// } = require('../controllers/paymentController');
-
-// router.post('/create-order', createOrder);
-// router.post('/verify-payment', verifyPayment);
-// router.get('/payment/:paymentId', getPaymentDetails);
-// router.post('/payment-failed', paymentFailed);
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/authMiddleware');
